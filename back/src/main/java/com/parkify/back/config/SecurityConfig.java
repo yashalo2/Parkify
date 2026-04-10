1db226e679b538a0576672396945d91735d0a9a6
Yasin5949 <yasin2ashalo@gmail.com>
2026-04-10 21:08:12 +0300

    configured the configuration page

--- back/src/main/java/com/parkify/back/config/SecurityConfig.java
+++ back/src/main/java/com/parkify/back/config/SecurityConfig.java
@@ -1,4 +1,43 @@
 package com.parkify.back.config;
 
-public class SecuriyConfig {
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/users/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.ALWAYS))
+
+
+                .cors(Customizer.withDefaults())
+        ;
+        return http.build();
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/**")
+                        .allowedOrigins("http://localhost:5173","http://10.240.212.124:5173/")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowCredentials(true);
+            }
+        };
+    }
 }
--- back/src/main/java/com/parkify/back/controller/UserController.java
+++ back/src/main/java/com/parkify/back/controller/UserController.java
@@ -1,4 +1,22 @@
 package com.parkify.back.controller;
 
+import com.parkify.back.model.User;
+import com.parkify.back.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/users")
 public class UserController {
+    @Autowired
+    private UserRepository userRepository;
+
+    @PostMapping("/register")
+    public String registerUser(@RequestBody User user) {
+        userRepository.save(user);
+        return "User registered successfully";
+    }
 }
